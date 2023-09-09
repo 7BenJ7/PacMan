251228e501f573998765c6104cda36cc72812fb4
MartinFond <97440783+MartinFond@users.noreply.github.com>
2023-09-09 22:40:31 +0200

    Ghost Random movements fixed + Eyes Anim

--- Assets/Scripts/Ghost/GhostAnimations.cs
+++ Assets/Scripts/Ghost/GhostAnimations.cs
@@ -7,6 +7,9 @@ public class GhostAnimations : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public UnityEngine.U2D.Animation.SpriteResolver spriteResolver;
+    public UnityEngine.U2D.Animation.SpriteResolver eyesSpriteResolver;
+
+    public GhostController ghost;
 
     private bool spriteState;
     private float animationsTimer;
@@ -18,7 +21,9 @@ public class GhostAnimations : MonoBehaviour
     {
         this.spriteRenderer = GetComponent<SpriteRenderer>();
         this.spriteResolver = GetComponent<UnityEngine.U2D.Animation.SpriteResolver>();
+        this.eyesSpriteResolver = transform.GetChild(0).GetComponent<UnityEngine.U2D.Animation.SpriteResolver>();
         this.spriteState = true;
+        ghost = GetComponent<GhostController>();
         animationsTimer = 0;
     }
 
@@ -36,6 +41,8 @@ public class GhostAnimations : MonoBehaviour
             animationsTimer = 0;
             UpdateSprite();
         }
+
+        UpdateEyes();
     }
 
     void UpdateSprite()
@@ -51,4 +58,26 @@ public class GhostAnimations : MonoBehaviour
 
         spriteState = !spriteState;
     }
+
+    void UpdateEyes()
+    {
+        switch (ghost.direction)
+        {
+            case var value when value == Vector2.up:
+                eyesSpriteResolver.SetCategoryAndLabel("Eyes", "Eyes Up");
+                break;
+            case var value when value == Vector2.down:
+                eyesSpriteResolver.SetCategoryAndLabel("Eyes", "Eyes Down");
+                break;
+            case var value when value == Vector2.left:
+                eyesSpriteResolver.SetCategoryAndLabel("Eyes", "Eyes Left");
+                break;
+            case var value when value == Vector2.right:
+                eyesSpriteResolver.SetCategoryAndLabel("Eyes", "Eyes Right");
+                break;
+            default:
+            break;
+        }
+    }
+    
 }
